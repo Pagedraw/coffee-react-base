2da18729f5822979f31903fbdbd46c65ccf0fb82
Jared Pochtar <jpochtar@gmail.com>
2018-01-25 13:57:34 -0800

    removed asset manifest
    I’m not entirely sure what this does

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -290,9 +290,9 @@ module.exports = {
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
-    new ManifestPlugin({
-      fileName: 'asset-manifest.json',
-    }),
+    // new ManifestPlugin({
+    //   fileName: 'asset-manifest.json',
+    // }),
     // Generate a service worker script that will precache, and keep up to date,
     // the HTML & assets that are part of the Webpack build.
     // new SWPrecacheWebpackPlugin({
