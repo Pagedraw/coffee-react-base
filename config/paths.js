60492bf9df9d243302b570b767706e37df458edf
Jared Pochtar <jpochtar@gmail.com>
2018-01-25 17:21:22 -0800

    use `static/` instead of `public/`

--- config/paths.js
+++ config/paths.js
@@ -42,8 +42,8 @@ function getServedPath(appPackageJson) {
 module.exports = {
   dotenv: resolveApp('.env'),
   appBuild: resolveApp('build'),
-  appPublic: resolveApp('public'),
-  appHtml: resolveApp('public/index.html'),
+  appPublic: resolveApp('static'),
+  appHtml: resolveApp('static/index.html'),
   appIndexJs: resolveApp('src/index.js'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
