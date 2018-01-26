8cf118e935e19881f2b8cb628fbb1c70e10b2157
Jared Pochtar <jpochtar@gmail.com>
2018-01-26 00:36:43 -0800

    /build/ -> /dist/

--- config/paths.js
+++ config/paths.js
@@ -41,7 +41,7 @@ function getServedPath(appPackageJson) {
 // config after eject: we're in ./config/
 module.exports = {
   dotenv: resolveApp('.env'),
-  appBuild: resolveApp('build'),
+  appBuild: resolveApp('dist'),
   appPublic: resolveApp('static'),
   appHtml: resolveApp('static/index.html'),
   appIndexJs: resolveApp('src/index.js'),
