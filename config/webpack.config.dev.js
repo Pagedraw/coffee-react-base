fe3bc81456907be555b40398b301496976f1169c
Jared Pochtar <jpochtar@gmail.com>
2018-01-26 03:03:12 -0800

    eject react-dev-utils/webpackHotDevClient

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -47,7 +47,7 @@ module.exports = {
     // the line below with these two lines if you prefer the stock client:
     // require.resolve('webpack-dev-server/client') + '?/',
     // require.resolve('webpack/hot/dev-server'),
-    require.resolve('react-dev-utils/webpackHotDevClient'),
+    require.resolve('./react-dev-utils/webpackHotDevClient'),
     // Finally, this is your app's code:
     jsPath,
     // We include the app code last so that if there is a runtime error during
